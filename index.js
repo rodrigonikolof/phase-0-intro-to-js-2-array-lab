3fb664026fb7cb5c44e06253e23a83825391e9e5
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-23 14:46:35 +1000

    Done with assignment for secont time :) refresher

--- index.js
+++ index.js
@@ -1,47 +1,93 @@
-const cats = [];
+// const cats = [];
 
-cats.push("Milo", "Otis", "Garfield");
-
-
+// cats.push("Milo", "Otis", "Garfield");
 
 
-function destructivelyAppendCat(name) {
-cats.push(name);
+
+
+// function destructivelyAppendCat(name) {
+// cats.push(name);
+// }
+
+// function destructivelyPrependCat(name) {
+// cats.unshift (name);
+// }
+
+// function destructivelyRemoveLastCat(name){
+//     cats.pop();
+// }
+
+// function destructivelyRemoveFirstCat(){
+//     cats.shift();
+// }
+
+// function appendCat(name){
+// const copyOfCats = [...cats];
+// copyOfCats.push (name);
+// return copyOfCats;
+
+// }
+
+// function prependCat(name){
+//     const copyOfCats = [...cats];
+//     copyOfCats.unshift (name);
+//     return copyOfCats;
+// }
+
+// function removeLastCat(name){
+//     const copyOfCats = [...cats];
+//     copyOfCats.pop ();
+//     return copyOfCats;
+// }
+
+// function removeFirstCat(name){
+//     const copyOfCats = [...cats];
+//     copyOfCats.shift ();
+//     return copyOfCats;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+     cats.push(name);
 }
-
-function destructivelyPrependCat(name) {
-cats.unshift (name);
+function destructivelyPrependCat(name){
+    cats.unshift(name);
 }
-
-function destructivelyRemoveLastCat(name){
+function destructivelyRemoveLastCat(){
     cats.pop();
 }
-
 function destructivelyRemoveFirstCat(){
     cats.shift();
 }
-
 function appendCat(name){
-const copyOfCats = [...cats];
-copyOfCats.push (name);
-return copyOfCats;
-
+    let moreCats = [...cats];
+    moreCats.push(name);
+    return moreCats;
 }
-
 function prependCat(name){
-    const copyOfCats = [...cats];
-    copyOfCats.unshift (name);
-    return copyOfCats;
+    let moreCats = [...cats];
+    moreCats.unshift(name);
+    return moreCats;
 }
-
-function removeLastCat(name){
-    const copyOfCats = [...cats];
-    copyOfCats.pop ();
-    return copyOfCats;
+function removeLastCat(){
+    let moreCats = [...cats];
+    moreCats.pop();
+    return moreCats; 
 }
-
-function removeFirstCat(name){
-    const copyOfCats = [...cats];
-    copyOfCats.shift ();
-    return copyOfCats;
+function removeFirstCat(){
+    let moreCats = [...cats];
+    moreCats.shift();
+    return moreCats; 
 }
